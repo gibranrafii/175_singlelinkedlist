b6bf7cb2f78f24e38f8d1fd406ed710acd1c523c
gibranrafii <gibranrafi21@gmail.com>
2024-04-25 09:41:16 +0700

    Membuat kondisi If

--- Praktikum 5.cpp
+++ Praktikum 5.cpp
@@ -70,7 +70,10 @@ bool searchNode(int nim, Node* current, Node* previous) {
 bool deleteNode(int nim) {
     Node* current = START;
     Node* previous = START;
-   
+    if (searchNode(nim, previous, current) ==
+        false)
+        return false;
+    previous->next = current->next;
 }
 
 int main()
